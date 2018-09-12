27135da44127d240f1f34da8aa37536a642c6b34
harry <hsauers5@gmail.com>
2018-09-11 22:39:19 -0400

    Fixed typo.

--- complete/src/main/java/restservice/LocationList.java
+++ complete/src/main/java/restservice/LocationList.java
@@ -6,7 +6,7 @@ public class LocationList extends HashMap {
 
     public LocationList() {
 
-        double[] gps = {28.5988385, -81.1990983}
+        double[] gps = {28.5988385, -81.1990983};
         addTrucks(0, gps);
 
         //populate list from database here
